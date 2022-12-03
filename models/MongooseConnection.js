eba4e434dc490532ec994d893ceb1d924838285e
Neeraj Mani <87220853+neeraj-mani@users.noreply.github.com>
2022-12-03 13:48:15 +0530

    Update MongooseConnection.js

--- models/MongooseConnection.js
+++ models/MongooseConnection.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const conn = mongoose.createConnection(
-  "mongodb+srv://nmt141319:rNImN2XevBe3jzAG@cluster0.vkhwchr.mongodb.net/?retryWrites=true&w=majority"
+  process.env.mongo_url
 );
 
 module.exports = conn;
